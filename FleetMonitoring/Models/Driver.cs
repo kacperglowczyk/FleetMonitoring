a305121fb493eea430587b00e8cebddc2540d66f
kacper.glowczyk <kacper.glowczyk@microsoft.wsei.edu.pl>
2023-08-14 16:10:50 +0200

    Major changes

--- FleetMonitoring/Models/Driver.cs
+++ FleetMonitoring/Models/Driver.cs
@@ -1,11 +1,47 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
+
 namespace FleetMonitoring.Models
 {
-	public class Driver
-	{
-		public Driver()
-		{
-		}
-	}
+    public class Driver
+    {
+        public int Id { get; set; }
+        public string DriverName { get; set; }
+        public string DriverSurname { get; set; }
+        public string RoadDriven { get; set; }
+    }
+
+    public class DriverContext : DbContext
+    {
+        public DbSet<Driver> Drivers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=Database.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Driver>()
+                .HasKey(d => d.Id);
+
+            modelBuilder.Entity<Driver>()
+                .Property(d => d.Id)
+                .ValueGeneratedOnAdd();
+
+            modelBuilder.Entity<Driver>()
+                .Property(d => d.DriverName)
+                .IsRequired();
+
+            modelBuilder.Entity<Driver>()
+                .Property(d => d.DriverSurname)
+                .IsRequired();
+
+            modelBuilder.Entity<Driver>()
+                .Property(d => d.RoadDriven)
+                .IsRequired();
+        }
+    }
+
 }
 
